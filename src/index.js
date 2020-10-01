604b3f0901cbb390df4b0bafc373553e5ec29341
Polina <70847449+pleuian@users.noreply.github.com>
2020-10-01 03:15:50 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-
 exports.min = function min (array) {
-  return 0;
+    var result = 0;
+
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    for(let i = 0; i < array.length; i++) {
+        if (result > array[i]) {
+            result = array[i];
+        }
+    }
+
+    return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    var result = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    for(let i = 0; i < array.length; i++) {
+        if (result < array[i]) {
+            result = array[i];
+        }
+    }
+
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    var result = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    var sum = 0;
+    for(let i = 0; i < array.length; i++) {
+        sum += array[i]
+        result = sum / array.length;
+    }
+
+  return result;
 }
